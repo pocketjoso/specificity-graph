fc7c86f5759da6097ea515b62aa1eb4b2b5eb567
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2014-11-16 20:07:21 +0000

    add prev/next focus functions

--- example.js
+++ example.js
@@ -9,6 +9,32 @@ var css = "body { font-size: 100%; }"+
           ".group { margin-bottom: 2em; }"+
           "#nav { display: block; }";
 
+// initialize the specificity graph
 specificityGraph.create(css, {
   svgSelector: '.js-graph'
+});
+
+
+
+// bind some events for stepping the graph focus
+document.addEventListener('keydown', function(evt){
+  if (evt.target.tagName == 'TEXTAREA'){
+    return;
+  }
+
+  if(evt.keyCode === 39 || evt.keyCode === 32){
+    specificityGraph.nextFocus();
+  } else if(evt.keyCode === 37){
+    specificityGraph.prevFocus();
+  }
+});
+
+var prev = document.querySelector('.js-prev'),
+    next = document.querySelector('.js-next');
+
+prev.addEventListener('click', function(evt){
+  specificityGraph.prevFocus();
+});
+next.addEventListener('click', function(evt){
+  specificityGraph.nextFocus();
 });
