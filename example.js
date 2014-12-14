7142199eefe5ff098604b86a8a6549538342cc9b
dhoko <aurelien@procheo.fr>
2014-12-14 19:18:29 +0100

    Upgrade chart generation

--- example.js
+++ example.js
@@ -5,15 +5,31 @@ var specificityGraph = require('./lib/core');
 //we're using d3 and drawing an SVG inside the DOM, this requires a browser.
 if(typeof document === 'undefined') return;
 
-var css = "body { font-size: 100%; }"+
-          ".group { margin-bottom: 2em; }"+
-          "#nav { display: block; }";
+function defaultExemple() {
+  var css = "body { font-size: 100%; }"+
+            ".group { margin-bottom: 2em; }"+
+            "#nav { display: block; }";
 
-// initialize the specificity graph
-specificityGraph.create(css, {
-  svgSelector: '.js-graph'
-});
+  // initialize the specificity graph
+  specificityGraph.create(css, {
+    svgSelector: '.js-graph'
+  });
+}
 
+var httpRequest = new XMLHttpRequest()
+httpRequest.onreadystatechange = function (data) {
+
+  if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+    var json = JSON.parse(httpRequest.responseText);
+    specificityGraph.draw(json,{
+      svgSelector: '.js-graph'
+    });
+  }else {
+    defaultExemple();
+  }
+}
+httpRequest.open('GET', 'specificity.json');
+httpRequest.send();
 
 
 // bind some events for stepping the graph focus
