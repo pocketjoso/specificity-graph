9a170a2b264fb49e64a385a4ba7ca2be5f51f130
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2015-02-21 14:41:33 +0000

    fix typos and formatting

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ var specificityGraph = require('./lib/core');
 //we're using d3 and drawing an SVG inside the DOM, this requires a browser.
 if(typeof document === 'undefined') return;
 
-function defaultExemple() {
+function defaultExample() {
   var css = "body { font-size: 100%; }"+
             ".group { margin-bottom: 2em; }"+
             "#nav { display: block; }";
